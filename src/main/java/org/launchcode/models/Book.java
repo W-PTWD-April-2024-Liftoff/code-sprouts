a0853481c01061f4bef78af2495ff5df1e1fcf8c
arcticcloudfox <27johnson279@gmail.com>
2025-03-18 14:08:48 -0500

    Added description and rating data variables

--- src/main/java/org/launchcode/models/Book.java
+++ src/main/java/org/launchcode/models/Book.java
@@ -1,18 +1,25 @@
 package org.launchcode.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Book extends AbstractEntity {
 
-    @NotBlank
+    @NotBlank(message = "Book name is required.")
     private String bookName;
 
+    @NotBlank(message = "Author is required.")
     private String author;
 
     private String category;
+
+    private int rating;
+
+    @NotBlank(message = "Description is required.")
+    @Size(max = 1000, message = "Description can be no more than 1000 characters,")
+    private String description;
 
     public String getBookName() {
         return bookName;
@@ -36,5 +43,21 @@ public class Book extends AbstractEntity {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
