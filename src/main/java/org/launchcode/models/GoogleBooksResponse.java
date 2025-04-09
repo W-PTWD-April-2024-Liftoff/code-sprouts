ea0a84c7e9b48761c7ce51e61d02d8fd9bd1eeaa
rahinasrahman <rshinasrahman94@gmail.com>
2025-04-09 08:35:47 -0500

    googleBookservice Class

---
+++ src/main/java/org/launchcode/models/GoogleBooksResponse.java
@@ -0,0 +1,93 @@
+package org.launchcode.models;
+
+import java.util.List;
+
+public class GoogleBooksResponse {
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public static class Item {
+        private VolumeInfo volumeInfo;
+
+        public VolumeInfo getVolumeInfo() {
+            return volumeInfo;
+        }
+
+        public void setVolumeInfo(VolumeInfo volumeInfo) {
+            this.volumeInfo = volumeInfo;
+        }
+    }
+
+    public static class VolumeInfo {
+        private String title;
+        private List<String> authors;
+        private String description;
+        private String category;
+        private String source;
+
+        public String getGenre() {
+            return genre;
+        }
+
+        public void setGenre(String genre) {
+            this.genre = genre;
+        }
+
+        private String genre;
+
+
+        public String getSource() {
+            return source;
+        }
+
+        public void setSource(String source) {
+            this.source = source;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+
+
+
+        // Getters and setters
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<String> getAuthors() {
+            return authors;
+        }
+
+        public void setAuthors(List<String> authors) {
+            this.authors = authors;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+
+    }
+}
